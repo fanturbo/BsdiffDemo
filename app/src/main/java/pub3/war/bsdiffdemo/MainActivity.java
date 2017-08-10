a7373c225c3329ced1c3a9ee366528e149a6d26d
fanturbo <615951217@qq.com>
2017-08-10 16:35:29 +0800

    添加权限；修改c文件；

--- app/src/main/java/pub3/war/bsdiffdemo/MainActivity.java
+++ app/src/main/java/pub3/war/bsdiffdemo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView) findViewById(R.id.tv);
-        tv.setText("Second");
+        tv.setText("233333");
     }
 
     public void checkVertion(View view) {
